12c0a57a1e612bd1f7009150498ba0bb75d58a7b
musawar <musawar1193@gmail.com>
2019-12-11 21:47:17 +0100

    dummy json data for shelters

---
+++ src/components/DummyData.js
@@ -0,0 +1,46 @@
+const shelters = [
+    {
+        'name': 'Obdachlosen e.V.',
+        'address': {
+            'street': 'Müllerstraße',
+            'number': 30,
+            'additional': 'Vorderhaus',
+            'plz': 13353,
+            'city': 'Berlin',
+            'geo': {
+                'lat': 52.543091,
+                'long': 13.364982
+            }
+
+        },
+        'email': 'Obdachlosen.ev@freenet.de',
+        'phone':{
+            'mobile': '0160 997865',
+            'home':'0160 997865'
+        },
+        'opening_hours':{
+            'from' : '6 pm',
+            'to': '9 am'
+        },
+        'sanitary_amenities':{
+            'wc': true,
+            'shower': false
+        },
+        'rules':{
+            'animals': false,
+            'shelter_seeking_person_intoxicated': false,
+            'female_only': false,
+            'families_welcome': false,
+            'male_only': false,
+            'kids_welcome': true
+        },
+        'spoken_languages':['English', 'German'],
+        'holder': 'church',
+        'number_of_beds':30,
+        'taken':20,
+        'intake_hours':{
+            'from': '6 pm',
+            'to': '22 pm'
+        }
+    }
+];
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,14 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import GlobalStyles from "../../styles/app-style";
 import Landing from "../../pages/Landing";
 import Navbar from "../../components/navbar/NavigationBar";
-import Geolocate from "../../components/DemoGeoLocate";
+
 import {UsePositionDemo} from "../../components/usePossitionDemo";
 import Map from "../../pages/Map";
 import Help from "../../pages/Help";
 import Page3 from "../../pages/Page3";
 import Page4 from "../../pages/Page4";
+
+import FindShelters from "../../pages/FindShelters";
 
 function App() {
   return (
@@ -21,11 +23,11 @@ function App() {
           path="/"
           render={routeProps => <Landing {...routeProps} />}
         />
+          <Route exact path="/find_shelters" component={FindShelters}/>
         {/*
         <Route exact path="/" component={}/>
         <Route exact path="/" component={}/>
         <Route exact path="/" component={}/> */}
-        <Route exact path="/gps" component={Geolocate}/>
         <Route exact path="/gpsdemo" component={UsePositionDemo}/>
         <Route exact path="/map" component={Map} />
         <Route exact path="/help" component={Help} />
