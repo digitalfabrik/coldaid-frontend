b362f3985af011a3e3334e0a442a6f1a80dc57e4
musawar <musawar1193@gmail.com>
2019-12-12 16:30:47 +0100

    issue #7 list view

--- src/components/FetchShelters.js
+++ src/components/FetchShelters.js
@@ -1,1 +1,11 @@
 import React from "react";
+import DummyData from "../components/DummyData";
+
+
+const FetchShelter = () =>{
+
+    console.log('fetch shelters function');
+    return DummyData;
+};
+
+export default FetchShelter;
