d11c6f7b181a7b44e0ae9299f3c4503035be1d67
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2020-01-06 10:24:20 +0100

    show live location on the map issue #9

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,7 @@ import GlobalStyles from "../../styles/app-style";
 import Landing from "../../pages/Landing";
 import Navbar from "../../components/navbar/NavigationBar";
 
-import Map from "../../pages/Map";
+import MapPage from "../../pages/Map";
 import Help from "../../pages/Help";
 import Page4 from "../../pages/Page4";
 
@@ -26,7 +26,7 @@ function App() {
         <Route exact path="/" component={}/>
         <Route exact path="/" component={}/>
         <Route exact path="/" component={}/> */}
-        <Route exact path="/map" component={Map} />
+        <Route exact path="/map" component={MapPage} />
         <Route exact path="/help" component={Help} />
         <Route exact path="/find_shelters" component={FindShelters} />
         <Route exact path="/page4" component={Page4} />
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {usePosition} from '../components/usePosition';
 import {useTranslation} from 'react-i18next';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
-export default function Map (){
-  const {t}                   = useTranslation();
-  const {latitude, longitude, error} = usePosition();
-  return (
-      <code>
-        {t('latitude')}: {latitude}<br/>
-        {t('longitude')}: {longitude}<br/>
-        error: {error}
-      </code>
-  );
+export default function MapPage () {
+
+    const {t}                   = useTranslation();
+    const {latitude, longitude, error} = usePosition();
+    let position = [52.52, 13.4];
+    if (latitude !== undefined && longitude !== undefined) {
+        position = [latitude.toFixed(2), longitude.toFixed(2)]
+    }
+    return (
+        <Map center={position} zoom={13} style={{height: "90vh"}}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </Map>
+    );
 };
